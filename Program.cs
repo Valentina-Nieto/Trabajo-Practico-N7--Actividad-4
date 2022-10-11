63bea2254864f3bb27c43cc9be2324a5262965b1
Valentina-Nieto <114711985+Valentina-Nieto@users.noreply.github.com>
2022-10-11 13:23:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace actividad4TP7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Realizar una aplicación que permita realizar la conversión de monedas DÓLAR y EURO a
+            PESO ARGENTINO. En primer lugar se debe solicitar el tipo de moneda origen (Dólar o Euro) y
+            la cantidad de la misma, luego se debe mostrar el monto correspondiente en Pesos Argentinos
+            y que se presione una tecla para permitir realizar una nueva conversión. Al momento de
+            seleccionar la moneda, también se debe brindar una opción para finalizar el programa.*/
+            
+            
+            float cotizacion = 0f, cotizacion2 = 0f;
+            float montoEnDolares = 0f;
+            float montoEnEuros = 0f;
+            float montoEnPesos = 0f, montoEnPesos2 = 0f;
+            string valor = "", opcion = "";
+            
+            while (!opcion.Contains("x") || !opcion.Contains("X") )
+            {
+                Console.WriteLine("Bienvenidx al Sitema de Conversión de Monedas");
+                Console.WriteLine("Presione 'D' si quiere hacer la conversion de Dólares a Pesos Argentinos");
+                Console.WriteLine("Presione 'E' si quiere hacer la conversion de Euros a Pesos Argentinos");
+                Console.WriteLine("Presione 'X' si quiere salir del programa");
+
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "d" or "D":
+
+                        Console.WriteLine("Elegimos el convertidor de Dólar a Pesos Argentinos");
+
+                        Console.WriteLine("Ingrese monto en Dólares");
+                        valor = Console.ReadLine();
+                        montoEnDolares = Convert.ToSingle(valor);
+
+                        Console.WriteLine("Ingrese la cotización del Dólar...");
+                        valor = Console.ReadLine();
+                        cotizacion = Convert.ToSingle(valor);
+                        montoEnPesos = montoEnDolares * cotizacion;
+
+                        Console.WriteLine("Su monto de Dólares es de ${0}", montoEnPesos);
+                        break;
+
+                    case "e" or "E":
+
+                        Console.WriteLine("Elegimos el convertidor de Euros a Pesos Argentinos");
+
+                        Console.WriteLine("Ingrese monto en Euros");
+                        valor = Console.ReadLine();
+                        montoEnEuros = Convert.ToSingle(valor);
+
+                        Console.WriteLine("Ingrese la cotización del Euro...");
+                        valor = Console.ReadLine();
+                        cotizacion2 = Convert.ToSingle(valor);
+                        montoEnPesos2 = montoEnEuros * cotizacion2;
+
+                        Console.WriteLine("Su monto de Euros es de ${0}", montoEnPesos2);
+                        break;
+
+                    case "x" or "X":
+                        Console.WriteLine("Salimos del Programa...");
+                        Console.ReadKey();
+                        break;
+                }
+                if (opcion != "" && !opcion.Contains("x") && !opcion.Contains("X"))
+                {
+                Console.WriteLine("Presione una tecla si 'i' quiere realizar otra conversion, o presione 'x' si quiere salir del programa");
+                opcion = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
